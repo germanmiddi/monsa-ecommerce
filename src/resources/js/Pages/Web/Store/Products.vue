3599f85dd7190148f60c8764e4783442bc57a807
German Middi <german@onmedia.com.ar>
2024-04-29 13:45:26 +1000

    correccion imagen en tienda

--- src/resources/js/Pages/Web/Store/Products.vue
+++ src/resources/js/Pages/Web/Store/Products.vue
@@ -51,17 +51,16 @@
     },
     methods: {
       buildImg(imagen) {
-        // console.log(imagen);
         let imageArray = JSON.parse(imagen);
-        // console.log(imageArray);
 
         if (imageArray.length > 0) {
-          const img = `https://www.monsa-srl.com.ar/pedidosweb${imageArray[0]}`;
-          return img;
+            // Asegura que la ruta de la imagen comience siempre con una barra '/'
+            const imagePath = imageArray[0].startsWith('/') ? imageArray[0] : '/' + imageArray[0];
+            const img = `https://www.monsa-srl.com.ar/pedidosweb${imagePath}`;
+            return img;
         } else {
-          return null;
+            return null;
         }
-       
       }
     }
   }
